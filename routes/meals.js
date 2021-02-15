96c401d1b4afa16503f152136879143bc7938962
Nihal <md.minhazuddin.nihal@gmail.com>
2021-02-15 11:42:06 +0600

    meal service api working

--- routes/meals.js
+++ routes/meals.js
@@ -1,59 +1,67 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const mealModel=require("../models/meals");
+const mealModel = require("../models/meals");
 /* GET home page. */
 
 let now = new Date();
 let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-let lastSunday = new Date(today.setDate(today.getDate()-today.getDay()+1));
-const perMeal=60;
+let lastSunday = new Date(today.setDate(today.getDate() - today.getDay() + 1));
+const perMeal = 60;
 
-router.get('/', async(req, res, next)=> {
-    const allData=await mealModel.find({});
+router.get("/", async (req, res, next) => {
+  const allData = await mealModel.find({});
   res.json({ allData });
 });
 
+router.post("/add", async (req, res) => {
+  const { userId, mealCount } = req.body;
 
+  const meal = new mealModel({
+    mealId: Date.now(),
+    userId,
+    mealCount,
+    price: mealCount * perMeal,
+    date:new Date()
+  });
 
-router.post("/add",async (req,res)=>{
-    const{userId,mealCount}=req.body;
+  let result = await meal.save();
+  res.json({ result, isAdded: true });
+});
 
-    const meal=new mealModel({
-        mealId:Date.now(),
-        userId,
-        mealCount,
-        price:(mealCount*perMeal),
-    });
-
-    let result=await meal.save();
-    res.json({result,isAdded:true});
-})
-
-
-
-router.get('/weekly',async(req,res)=>{
-    const pipeline=[
-        {$match:{
-            date:{$gte:lastSunday,$lte:today}
-        }},
+router.get("/weekly", async (req, res) => {
+  const pipeline = [
     {
-        $group:{
-            _id:{userId:"$userId"},
-            
-        }
+      $match: {
+        date: { $gte: lastSunday },
+      },
     },
     {
-        $lookup:{
-            from:"users",
-            localField:"userId",
-            foreignField:"userId",
-            as:"user"
+      $group: {
+        _id: { userId: "$userId" },
+        totalMeal:{
+            $sum:"$mealCount"
+        },
+        totalPrice:{
+            $sum:"$price"
         }
-    }
-    ];
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        let:{uId:"$_id.userId"},
+        pipeline:[{
+            $match:{
+                $expr:{$eq:["$userId","$$uId"]}
+            }
+        }],
+        as: "user",
+      },
+    },
+  ];
 
-    const weeklyData=await mealModel.aggregate(pipeline);
-    res.json({weeklyData});
-})
+  const weeklyData = await mealModel.aggregate(pipeline);
+  res.json({ weeklyData });
+});
 
 module.exports = router;
